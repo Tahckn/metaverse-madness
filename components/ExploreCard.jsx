e215ea8fc2357dda7ffaefe49b2aa5fae60ec4bf
Taha Ceken <taha-ceken@hotmail.com>
2023-04-24 13:59:07 +0300

    get started section done

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -32,7 +32,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
         <p className='font-normal text-[16px] leading-[20px] text-white uppercase'>
           Enter the Metaverse
         </p>
-        <h2 className='mt-[24px] font-semibold sm:text-[32px] text-white text-[24px] text-white'>
+        <h2 className='mt-[24px] font-semibold sm:text-[32px] text-white text-[24px] '>
           {title}
         </h2>
       </div>
